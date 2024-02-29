b4b4f2c35f740ecbc2d1eb9c6a2381d19af8ab8d
Gopal Raju <dommarajugopal6@gmail.com>
2024-02-29 10:22:46 +0530

    Initial Commit

---
+++ src/main/java/com/studentdata/clgstudentdata/ClgStudentDataApplication.java
@@ -0,0 +1,13 @@
+package com.studentdata.clgstudentdata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClgStudentDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClgStudentDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/studentdata/clgstudentdata/ClgStudentDataApplicationTests.java
@@ -0,0 +1,13 @@
+package com.studentdata.clgstudentdata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClgStudentDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
